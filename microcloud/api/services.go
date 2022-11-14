c9633c6f04a71743845aadbc3b862af32cbb63f6
Max Asnaashari <max.asnaashari@canonical.com>
2022-11-14 20:34:10 +0000

    microcloud/api/services: Update proxy handlers
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- microcloud/api/services.go
+++ microcloud/api/services.go
@@ -11,6 +11,7 @@ import (
 	"github.com/canonical/microcluster/state"
 	"github.com/lxc/lxd/client"
 	"github.com/lxc/lxd/lxd/response"
+	"github.com/lxc/lxd/shared"
 )
 
 // LXDProxy proxies all requests from MicroCloud to LXD.
@@ -18,6 +19,12 @@ var LXDProxy = Proxy("lxd", "services/lxd/{rest:.*}", lxdHandler)
 
 // CephProxy proxies all requests from MicroCloud to MicroCeph.
 var CephProxy = Proxy("microceph", "services/microceph/{rest:.*}", cephHandler)
+
+// LXDDir is the path to the state directory of the LXD snap.
+const LXDDir = "/var/snap/lxd/common/lxd"
+
+// MicroCephDir is the path to the state directory of the MicroCeph snap.
+const MicroCephDir = "/var/snap/microceph/common/state"
 
 // Proxy returns a proxy endpoint with the given handler and access applied to all REST methods.
 func Proxy(name, path string, handler func(*state.State, *http.Request) response.Response) rest.Endpoint {
@@ -38,6 +45,34 @@ func lxdHandler(s *state.State, r *http.Request) response.Response {
 	_, path, ok := strings.Cut(r.URL.Path, "/1.0/services/lxd")
 	if !ok {
 		return response.SmartError(fmt.Errorf("Invalid path %q", r.URL.Path))
+	}
+
+	if r.Header.Get("Upgrade") == "websocket" {
+		client, err := lxd.ConnectLXDUnix(filepath.Join(LXDDir, "unix.socket"), nil)
+		if err != nil {
+			return response.SmartError(fmt.Errorf("Failed to connect to local LXD: %w", err))
+		}
+
+		// RawWebsocket assigns /1.0, so remove it here.
+		_, path, _ = strings.Cut(path, "/1.0")
+		ws, err := client.RawWebsocket(path)
+		if err != nil {
+			return response.SmartError(err)
+		}
+
+		// Perform the websocket proxy.
+		return response.ManualResponse(func(w http.ResponseWriter) error {
+			conn, err := shared.WebsocketUpgrader.Upgrade(w, r, nil)
+			if err != nil {
+				return err
+			}
+
+			defer conn.Close()
+
+			<-shared.WebsocketProxy(ws, conn)
+
+			return nil
+		})
 	}
 
 	// Must unset the RequestURI. It is an error to set this in a client request.
@@ -72,7 +107,7 @@ func cephHandler(s *state.State, r *http.Request) response.Response {
 	r.URL.Scheme = "http"
 	r.URL.Host = filepath.Join(MicroCephDir, "control.socket")
 	r.Host = r.URL.Host
-	client, err := microcluster.App(s.Context, MicroCephDir, false, false)
+	client, err := microcluster.App(s.Context, microcluster.Args{StateDir: MicroCephDir})
 	if err != nil {
 		return response.SmartError(err)
 	}
