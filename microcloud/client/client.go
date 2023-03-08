afe35dc3ad4b3291ca5d0859f62cb94afbb62c6f
Max Asnaashari <max.asnaashari@canonical.com>
2023-03-08 05:16:23 +0000

    microcloud/client/client: Add /1.0/services client helper
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ microcloud/client/client.go
@@ -0,0 +1,24 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/canonical/microcloud/microcloud/api/types"
+	"github.com/canonical/microcluster/client"
+	"github.com/lxc/lxd/shared/api"
+)
+
+// JoinServices sends join information to initiate the cluster join process.
+func JoinServices(ctx context.Context, c *client.Client, data types.ServicesPut) error {
+	queryCtx, cancel := context.WithTimeout(ctx, time.Second*30)
+	defer cancel()
+
+	err := c.Query(queryCtx, "PUT", api.NewURL().Path("services"), data, nil)
+	if err != nil {
+		return fmt.Errorf("Failed to update cluster status of services: %w", err)
+	}
+
+	return nil
+}
