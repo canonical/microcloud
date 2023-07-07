629087dea93dcba3a23a58f79cbb28d8cfd6f59d
Mark Laing <mark.laing@canonical.com>
2023-07-07 12:07:49 +0100

    cmd: Fixes lint errors.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- microcloud/cmd/microcloud/add.go
+++ microcloud/cmd/microcloud/add.go
@@ -67,7 +67,7 @@ func (c *cmdAdd) Run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	s, err := service.NewServiceHandler(status.Name, addr, c.common.FlagMicroCloudDir, c.common.FlagLogDebug, c.common.FlagLogVerbose, services...)
+	s, err := service.NewHandler(status.Name, addr, c.common.FlagMicroCloudDir, c.common.FlagLogDebug, c.common.FlagLogVerbose, services...)
 	if err != nil {
 		return err
 	}
