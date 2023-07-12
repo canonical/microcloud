c0c0becc4df34fed59fc65a1fe32c7cf3da28bc2
Mark Laing <mark.laing@canonical.com>
2023-07-12 14:04:28 +0100

    cmd/microcloud: Adds app command as child to main command.
    Signed-off-by: Mark Laing <mark.laing@canonical.com>

--- microcloud/cmd/microcloud/main.go
+++ microcloud/cmd/microcloud/main.go
@@ -69,6 +69,9 @@ func main() {
 	var cmdWaitready = cmdWaitready{common: &commonCmd}
 	app.AddCommand(cmdWaitready.Command())
 
+	var cmdApps = cmdApps{common: &commonCmd}
+	app.AddCommand(cmdApps.Command())
+
 	app.InitDefaultHelpCmd()
 
 	err := app.Execute()
