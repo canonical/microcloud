759694fc3f635ad2dd0d074559957ef673b92d85
Max Asnaashari <max.asnaashari@canonical.com>
2023-08-28 15:42:48 +0000

    service/lxd: Prevent LXD restart failing if in progress
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- microcloud/service/lxd.go
+++ microcloud/service/lxd.go
@@ -400,7 +400,7 @@ func (s *LXDService) Restart(timeoutSeconds int) error {
 	}
 
 	_, _, err = c.RawQuery("PUT", "/internal/shutdown", nil, "")
-	if err != nil {
+	if err != nil && err.Error() != "Shutdown already in progress" {
 		return fmt.Errorf("Failed to send shutdown request to LXD: %w", err)
 	}
 
