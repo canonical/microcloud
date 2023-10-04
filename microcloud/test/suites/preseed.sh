ae9b491d3c5e283f854f72b0274cc0b4ff12f6d1
Max Asnaashari <max.asnaashari@canonical.com>
2023-10-04 19:31:26 +0000

    microcloud/test/suites: Add tests for FAN network with preseed
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- microcloud/test/suites/preseed.sh
+++ microcloud/test/suites/preseed.sh
@@ -83,4 +83,47 @@ EOF
   validate_system_lxd micro04 4 disk1 1 enp6s0 10.1.123.1/24 10.1.123.100-10.1.123.254 fd42:1:1234:1234::1/64
   validate_system_microceph micro04 disk2
   validate_system_microovn micro04
+
+  reset_systems 3 3 2
+  addr=$(lxc ls micro01 -f csv -c4 | grep enp5s0 | cut -d' ' -f1)
+
+  lxc exec micro01 -- sh -c "
+  cat << EOF > /root/preseed.yaml
+lookup_subnet: ${addr}/24
+systems:
+- name: micro01
+- name: micro02
+- name: micro03
+EOF
+"
+
+  lxc exec micro01 -- sh -c "TEST_CONSOLE=0 microcloud init --preseed /root/preseed.yaml"
+  for m in micro01 micro02 micro03 ; do
+    validate_system_lxd ${m} 3
+    validate_system_microceph ${m}
+    validate_system_microovn ${m}
+  done
+
+  reset_systems 3 3 2
+  addr=$(lxc ls micro01 -f csv -c4 | grep enp5s0 | cut -d' ' -f1)
+
+  lxc exec micro01 -- sh -c "
+  snap disable microceph
+  snap disable microovn
+
+  sleep 1
+
+  cat << EOF > /root/preseed.yaml
+lookup_subnet: ${addr}/24
+systems:
+- name: micro01
+- name: micro02
+- name: micro03
+EOF
+"
+
+  lxc exec micro01 -- sh -c "TEST_CONSOLE=0 microcloud init --preseed /root/preseed.yaml"
+  for m in micro01 micro02 micro03 ; do
+    validate_system_lxd ${m} 3
+  done
 }
