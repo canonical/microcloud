f937096ebb5067f906754f5bef92fbb743c7ea9e
Max Asnaashari <max.asnaashari@canonical.com>
2024-01-26 03:48:19 +0000

    microcloud/test/suites: Add reuse-cluster tests
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- microcloud/test/main.sh
+++ microcloud/test/main.sh
@@ -144,6 +144,7 @@ if [ "${1:-"all"}" != "cluster" ]; then
   run_test test_add_interactive "add interactive"
   run_test test_add_auto "add auto"
   run_test test_preseed "preseed"
+  run_test test_reuse_cluster "reuse_cluster"
 fi
 
 # shellcheck disable=SC2034
