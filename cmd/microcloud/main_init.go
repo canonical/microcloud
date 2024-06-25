7c2daca2243fa9f85b62602be6376b7f84cd0512
Max Asnaashari <max.asnaashari@canonical.com>
2024-06-25 18:29:57 +0000

    cmd/microcloud: Use initConfig as validateSystems receiver
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- cmd/microcloud/main_init.go
+++ cmd/microcloud/main_init.go
@@ -630,9 +630,8 @@ func validateGatewayNet(config map[string]string, ipPrefix string, cidrValidator
 	return ovnIPRanges, nil
 }
 
-func validateSystems(s *service.Handler, systems map[string]InitSystem) (err error) {
-	curSystem, bootstrap := systems[s.Name]
-	if !bootstrap {
+func (c *initConfig) validateSystems(s *service.Handler) (err error) {
+	if !c.bootstrap {
 		return nil
 	}
 
@@ -641,7 +640,7 @@ func validateSystems(s *service.Handler, systems map[string]InitSystem) (err err
 	// systems' management addrs
 	var ip4OVNRanges, ip6OVNRanges []*shared.IPRange
 
-	for _, network := range curSystem.Networks {
+	for _, network := range c.systems[s.Name].Networks {
 		if network.Type == "physical" && network.Name == service.DefaultUplinkNetwork {
 			ip4OVNRanges, err = validateGatewayNet(network.Config, "ipv4", validate.IsNetworkAddressCIDRV4)
 			if err != nil {
@@ -676,7 +675,7 @@ func validateSystems(s *service.Handler, systems map[string]InitSystem) (err err
 
 	// Ensure that no system's management address falls within the OVN ranges
 	// to prevent OVN from allocating an IP that's already in use.
-	for systemName, system := range systems {
+	for systemName, system := range c.systems {
 		// If the system is ourselves, we don't have an mDNS payload so grab the address locally.
 		addr := system.ServerInfo.Address
 		if systemName == s.Name {
