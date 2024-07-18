408c29e137ab369eab353e7026747670c8a6e220
Max Asnaashari <max.asnaashari@canonical.com>
2024-07-18 22:31:45 +0000

    cmd/microcloud: Add microcloud remove <name>
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ cmd/microcloud/remove.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+
+	"github.com/canonical/microcluster/microcluster"
+	"github.com/spf13/cobra"
+
+	cloudClient "github.com/canonical/microcloud/microcloud/client"
+)
+
+type cmdRemove struct {
+	common *CmdControl
+
+	flagForce bool
+}
+
+func (c *cmdRemove) Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "remove <name>",
+		Aliases: []string{"rm"},
+		Short:   "Remove the specified member from all MicroCloud services",
+		RunE:    c.Run,
+	}
+
+	cmd.Flags().BoolVarP(&c.flagForce, "force", "f", false, "Forcibly remove the cluster member")
+
+	return cmd
+}
+
+func (c *cmdRemove) Run(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return cmd.Help()
+	}
+
+	options := microcluster.Args{StateDir: c.common.FlagMicroCloudDir, Verbose: c.common.FlagLogVerbose, Debug: c.common.FlagLogDebug}
+	m, err := microcluster.App(options)
+	if err != nil {
+		return err
+	}
+
+	client, err := m.LocalClient()
+	if err != nil {
+		return err
+	}
+
+	return cloudClient.DeleteClusterMember(context.Background(), client, args[0], c.flagForce)
+}
