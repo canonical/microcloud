cc9b1efec76d86ce409ecacbe8df040d02fffdeb
Max Asnaashari <max.asnaashari@canonical.com>
2024-08-16 04:43:53 +0000

    test/suites: Add storage & network creation test
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- test/suites/add.sh
+++ test/suites/add.sh
@@ -198,4 +198,56 @@ test_add_interactive() {
   for m in micro01 micro02 micro03 micro04 ; do
     validate_system_lxd "${m}" 4 disk1
   done
+
+  reset_systems 4 2 1
+  echo "Test growing a MicroCloud when storage & networks were not already set up"
+  unset_interactive_vars
+  export LOOKUP_IFACE="enp5s0"
+  export LIMIT_SUBNET="yes"
+  export EXPECT_PEERS=2
+  export SETUP_ZFS="no"
+  export SETUP_CEPH="no"
+  export SETUP_OVN="no"
+
+  lxc exec micro04 -- snap disable microcloud
+  microcloud_interactive | lxc exec micro01 -- sh -c "microcloud init > out"
+  lxc exec micro01 -- tail -1 out | grep "MicroCloud is ready" -q
+
+  for m in micro01 micro02 micro03; do
+    validate_system_lxd "${m}" 3
+    validate_system_microceph "${m}"
+    validate_system_microovn "${m}"
+  done
+
+  unset_interactive_vars
+  export LIMIT_SUBNET="yes"
+  export EXPECT_PEERS=1
+  export SETUP_ZFS="yes"
+  export ZFS_FILTER="lxd_disk1"
+  export ZFS_WIPE="yes"
+  export SETUP_CEPH="yes"
+  export SETUP_CEPHFS="yes"
+  export CEPH_WIPE="yes"
+  export CEPH_ENCRYPT="no"
+  export SETUP_OVN="yes"
+  export OVN_FILTER="enp6s0"
+  export IPV4_SUBNET="10.1.123.1/24"
+  export IPV4_START="10.1.123.100"
+  export IPV4_END="10.1.123.254"
+  export IPV6_SUBNET="fd42:1:1234:1234::1/64"
+  export DNS_ADDRESSES="10.1.123.1,fd42:1:1234:1234::1"
+  export REPLACE_PROFILE="yes"
+
+  lxc exec micro04 -- snap enable microcloud
+  lxc exec micro04 -- snap start microcloud
+  microcloud_interactive | lxc exec micro01 -- sh -c "microcloud add > out"
+
+  lxc exec micro01 -- tail -1 out | grep "MicroCloud is ready" -q
+
+  default_cluster_subnet="$(lxc exec micro01 -- ip -4 -br a show enp5s0 | awk '{print $3}')"
+  for m in micro01 micro02 micro03 micro04 ; do
+    validate_system_lxd "${m}" 4 disk1 1 1 enp6s0 10.1.123.1/24 10.1.123.100-10.1.123.254 fd42:1:1234:1234::1/64  10.1.123.1,fd42:1:1234:1234::1
+    validate_system_microceph "${m}" 1 "${default_cluster_subnet}" disk2
+    validate_system_microovn "${m}"
+  done
 }
