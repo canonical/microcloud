237829d2cb84a3dd6504aa87919e657e3384dd64
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-08-26 14:28:40 +0200

    mdns: Lookup only a single system and increase the version number
    This reworks the mDNS code to support looking up only a single system over the configured
    lookup interface which matches the one used by the local MicroCloud's listen address.
    It has the added effect that we can also get rid of the Interface field in the ServerInfo struct.
    
    In addition the broadcast version number is increased to prevent collissions with earlier versions of MicroCloud.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- mdns/lookup.go
+++ mdns/lookup.go
@@ -2,6 +2,7 @@ package mdns
 
 import (
 	"context"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -18,12 +19,11 @@ import (
 
 // ServerInfo is information about the server that is broadcast over mDNS.
 type ServerInfo struct {
-	Version    string
-	Name       string
-	Address    string
-	Interface  string
-	Services   []types.ServiceType
-	AuthSecret string
+	Version     string
+	Name        string
+	Address     string
+	Services    []types.ServiceType
+	Certificate *x509.Certificate
 }
 
 // NetworkInfo represents information about a network interface broadcast by a MicroCloud peer.
@@ -31,11 +31,6 @@ type NetworkInfo struct {
 	Interface net.Interface
 	Address   string
 	Subnet    *net.IPNet
-}
-
-// LookupKey returns a unique key representing a lookup entry.
-func (s ServerInfo) LookupKey() string {
-	return fmt.Sprintf("%s_%s_%s", s.Name, s.Interface, s.Address)
 }
 
 // forwardingWriter forwards the mdns log message to LXD's logger package.
@@ -57,92 +52,57 @@ func (f forwardingWriter) Write(p []byte) (int, error) {
 	return len(logMsg), nil
 }
 
-// LookupPeers finds any broadcasting peers and returns a list of their names.
-func LookupPeers(ctx context.Context, iface *net.Interface, version string, localPeer string) (map[string]ServerInfo, error) {
-	entries := []*mdns.ServiceEntry{}
-	for i := 0; i < ServiceSize; i++ {
-		nextEntries, err := Lookup(ctx, iface, fmt.Sprintf("%s_%d", ClusterService, i), clusterSize)
-		if err != nil {
-			return nil, err
-		}
-
-		entries = append(entries, nextEntries...)
+// LookupPeer finds any broadcasting peer and returns its info.
+func LookupPeer(ctx context.Context, iface *net.Interface, version string) (*ServerInfo, error) {
+	entry, err := Lookup(ctx, iface, ClusterService)
+	if err != nil {
+		return nil, err
 	}
 
-	peers := map[string]ServerInfo{}
-	for _, entry := range entries {
-		if entry == nil {
-			return nil, fmt.Errorf("Received empty record")
-		}
-
-		if !strings.HasSuffix(entry.Name, ".local.") {
-			continue
-		}
-
-		serviceStr := strings.TrimSuffix(entry.Name, ".local.")
-		parts := strings.SplitN(serviceStr, fmt.Sprintf(".%s_", ClusterService), 2)
-		if len(parts) != 2 {
-			continue
-		}
-
-		// Skip a response from ourselves.
-		peerName := parts[0]
-		if localPeer == peerName {
-			continue
-		}
-
-		if len(entry.InfoFields) == 0 {
-			logger.Infof("Received incomplete record from peer %q", peerName)
-			continue
-		}
-
-		unquoted, err := strconv.Unquote("\"" + strings.Join(entry.InfoFields, "") + "\"")
-		if err != nil {
-			return nil, fmt.Errorf("Failed to format DNS TXT record: %w", err)
-		}
-
-		info := ServerInfo{}
-		err = json.Unmarshal([]byte(unquoted), &info)
-		if err != nil {
-			return nil, fmt.Errorf("Failed to parse server info: %w", err)
-		}
-
-		// Skip a response from ourselves.
-		if localPeer == info.Name {
-			continue
-		}
-
-		// Skip any responses from mismatched versions.
-		if info.Version != version {
-			logger.Infof("System %q (version %q) has a version mismatch. Expected %q", peerName, info.Version, version)
-			continue
-		}
-
-		peers[info.LookupKey()] = info
+	if entry == nil {
+		return nil, fmt.Errorf("Received empty record")
 	}
 
-	return peers, nil
+	if !strings.HasSuffix(entry.Name, ".local.") {
+		return nil, fmt.Errorf("Invalid service name %q", entry.Name)
+	}
+
+	serviceStr := strings.TrimSuffix(entry.Name, ".local.")
+	parts := strings.SplitN(serviceStr, ClusterService, 2)
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("Invalid service name %q", entry.Name)
+	}
+
+	peerName := parts[0]
+	if len(entry.InfoFields) == 0 {
+		return nil, fmt.Errorf("Received incomplete record from peer %q", peerName)
+	}
+
+	unquoted, err := strconv.Unquote("\"" + strings.Join(entry.InfoFields, "") + "\"")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to format DNS TXT record: %w", err)
+	}
+
+	info := ServerInfo{}
+	err = json.Unmarshal([]byte(unquoted), &info)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse server info: %w", err)
+	}
+
+	// Skip any responses from mismatched versions.
+	if info.Version != version {
+		return nil, fmt.Errorf("System %q (version %q) has a version mismatch. Expected %q", peerName, info.Version, version)
+	}
+
+	return &info, nil
 }
 
 // Lookup searches for the given service name over mdns.
-func Lookup(ctx context.Context, iface *net.Interface, service string, size int) ([]*mdns.ServiceEntry, error) {
+func Lookup(ctx context.Context, iface *net.Interface, service string) (*mdns.ServiceEntry, error) {
 	log.SetOutput(forwardingWriter{})
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	entriesCh := make(chan *mdns.ServiceEntry, size)
-	entries := []*mdns.ServiceEntry{}
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				for entry := range entriesCh {
-					entries = append(entries, entry)
-				}
-			}
-		}
-	}()
+
+	entriesCh := make(chan *mdns.ServiceEntry, 1)
+	defer close(entriesCh)
 
 	params := mdns.DefaultParams(service)
 	params.Interface = iface
@@ -167,14 +127,20 @@ func Lookup(ctx context.Context, iface *net.Interface, service string, size int)
 		return nil, fmt.Errorf("No supported IP versions on the network interface %q", iface.Name)
 	}
 
-	err = mdns.Query(params)
-	if err != nil {
-		return nil, fmt.Errorf("Failed lookup: %w", err)
+	// Return the first peer that gets found.
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, fmt.Errorf("Failed lookup: %w", ctx.Err())
+		case entry := <-entriesCh:
+			return entry, nil
+		default:
+			err = mdns.Query(params)
+			if err != nil {
+				return nil, fmt.Errorf("Failed lookup: %w", err)
+			}
+		}
 	}
-
-	close(entriesCh)
-
-	return entries, nil
 }
 
 // GetNetworkInfo returns a slice of NetworkInfo to be included in the mDNS broadcast.
