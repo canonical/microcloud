b53cea236e4a901baa0e6616cf381b0bb003c7ce
Max Asnaashari <max.asnaashari@canonical.com>
2024-10-10 17:45:48 +0000

    api/types: Record version with Services
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- api/types/session.go
+++ api/types/session.go
@@ -18,25 +18,25 @@ const (
 // Session represents the websocket protocol used during trust establishment between the client and server.
 // Empty fields are omitted to require sending only the necessary information.
 type Session struct {
-	Address              string            `json:"address,omitempty"`
-	InitiatorAddress     string            `json:"initiator_address,omitempty"`
-	InitiatorName        string            `json:"initiator_name,omitempty"`
-	InitiatorFingerprint string            `json:"initiator_fingerprint,omitempty"`
-	Interface            string            `json:"interface,omitempty"`
-	Passphrase           string            `json:"passphrase,omitempty"`
-	Services             []ServiceType     `json:"services,omitempty"`
-	Intent               SessionJoinPost   `json:"intent,omitempty"`
-	ConfirmedIntents     []SessionJoinPost `json:"confirmed_intents,omitempty"`
-	Accepted             bool              `json:"accepted,omitempty"`
-	LookupTimeout        time.Duration     `json:"lookup_timeout,omitempty"`
-	Error                string            `json:"error,omitempty"`
+	Address              string                 `json:"address,omitempty"`
+	InitiatorAddress     string                 `json:"initiator_address,omitempty"`
+	InitiatorName        string                 `json:"initiator_name,omitempty"`
+	InitiatorFingerprint string                 `json:"initiator_fingerprint,omitempty"`
+	Interface            string                 `json:"interface,omitempty"`
+	Passphrase           string                 `json:"passphrase,omitempty"`
+	Services             map[ServiceType]string `json:"services,omitempty"`
+	Intent               SessionJoinPost        `json:"intent,omitempty"`
+	ConfirmedIntents     []SessionJoinPost      `json:"confirmed_intents,omitempty"`
+	Accepted             bool                   `json:"accepted,omitempty"`
+	LookupTimeout        time.Duration          `json:"lookup_timeout,omitempty"`
+	Error                string                 `json:"error,omitempty"`
 }
 
 // SessionJoinPost represents a request made to join an active session.
 type SessionJoinPost struct {
-	Name        string        `json:"name" yaml:"name"`
-	Version     string        `json:"version" yaml:"version"`
-	Address     string        `json:"address" yaml:"address"`
-	Certificate string        `json:"certificate" yaml:"certificate"`
-	Services    []ServiceType `json:"services" yaml:"services"`
+	Name        string                 `json:"name" yaml:"name"`
+	Version     string                 `json:"version" yaml:"version"`
+	Address     string                 `json:"address" yaml:"address"`
+	Certificate string                 `json:"certificate" yaml:"certificate"`
+	Services    map[ServiceType]string `json:"services" yaml:"services"`
 }
