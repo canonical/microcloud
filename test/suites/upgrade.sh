bd7636511c10a06626b56307a33e43ed4fb68aac
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-11-14 11:37:15 +0100

    test/suites/upgrade: Pin down dependencies
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- test/suites/upgrade.sh
+++ test/suites/upgrade.sh
@@ -5,10 +5,10 @@ test_upgrade() {
 
   # Perform upgrade test from MicroCloud 1 to 2.
   if [ "${MICROCLOUD_SNAP_CHANNEL}" = "1/stable" ]; then
-    microceph_target="squid/edge" # TODO: squid/stable when available
-    microovn_target="latest/edge" # TODO: 24.03/stable when available
-    lxd_target="5.21/edge"
-    microcloud_target="latest/edge" # TODO: 2/stable when available
+    microceph_target="squid/stable"
+    microovn_target="24.03/stable"
+    lxd_target="5.21/stable"
+    microcloud_target="2/edge"
 
     # The lookup subnet has to contain the netmask and the address has to be the one used by MicroCloud not the gateway.
     lookup_subnet="$(lxc ls micro01 -f json -c4 | jq -r '.[0].state.network.enp5s0.addresses[] | select(.family == "inet") | .address + "/" + .netmask')"
