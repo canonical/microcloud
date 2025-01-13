93b2ca03874a7de2fee3ed7eaa8eecf46b9231e3
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-01-13 09:37:22 +0100

    test/suites/upgrade: Use the respective service's candidate channels
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    (cherry picked from commit a8994e99bbb576a2b6ac61b73bba5abade4029cb)

--- test/suites/upgrade.sh
+++ test/suites/upgrade.sh
@@ -21,10 +21,11 @@ test_upgrade() {
 
   # Perform upgrade test from MicroCloud 1 to 2.
   if [ "${MICROCLOUD_SNAP_CHANNEL}" = "1/candidate" ]; then
-    microceph_target="squid/stable"
-    microovn_target="24.03/stable"
-    lxd_target="5.21/stable"
-    microcloud_target="2/edge"
+    # Use the candidate channels to catch issues early in the release process.
+    microceph_target="squid/candidate"
+    microovn_target="24.03/candidate"
+    lxd_target="5.21/candidate"
+    microcloud_target="2/candidate"
 
     # The lookup subnet has to contain the netmask and the address has to be the one used by MicroCloud not the gateway.
     lookup_subnet="$(lxc ls micro01 -f json -c4 | jq -r '.[0].state.network.enp5s0.addresses[] | select(.family == "inet") | .address + "/" + .netmask')"
