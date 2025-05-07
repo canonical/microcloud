1ebeeb07f4f4fef22eea0d53c52c7866c1133099
Minae Lee <minae.lee@canonical.com>
2025-05-07 13:14:55 -0700

    update absolute URL links to LXD docs
    Signed-off-by: Minae Lee <minae.lee@canonical.com>

--- doc/.sphinx/_integration/add_config.py
+++ doc/.sphinx/_integration/add_config.py
@@ -13,7 +13,7 @@ html_context['microovn_tag'] = "../microovn/_static/microovn.png"
 custom_html_js_files.append('rtd-search.js')
 
 if project == "LXD":
-    html_baseurl = "https://documentation.ubuntu.com/lxd/en/latest/"
+    html_baseurl = "https://documentation.ubuntu.com/lxd/latest/"
 elif project == "MicroCeph":
     html_baseurl = "https://canonical-microceph.readthedocs-hosted.com/en/latest/"
 elif project == "MicroOVN":
