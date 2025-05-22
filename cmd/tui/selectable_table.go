0ecd50a61160772036983695d6ad8d18736c0a0a
David Edler <david.edler@canonical.com>
2025-05-22 11:49:56 +0200

    cmd/tui/handler: simplify to countAllRows as we only ever read the size of the rows and don't need a copy of the table rows. This avoids a kernel panic, suspected due to parallel processing of handleRemoveEvent or handleInsertEvent on the table
    Signed-off-by: David Edler <david.edler@canonical.com>
    (cherry picked from commit aab8227b4075d72a8d8353c2dd404ff35b04acc5)

--- cmd/tui/selectable_table.go
+++ cmd/tui/selectable_table.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -36,6 +37,7 @@ const defaultTableSize = 10
 
 type selectableTable struct {
 	table   *table.Table
+	tableMu sync.Mutex
 	program *tea.Program
 
 	// size is the number of rows that will be displayed at once.
@@ -112,7 +114,7 @@ func (s *selectableTable) Render(ctx context.Context, handler *InputHandler, tit
 		return nil, errors.New("Cannot render table while another is already active")
 	}
 
-	handler.tableMu.Lock()
+	s.tableMu.Lock()
 	s.active = true
 	s.title = title
 	s.testMode = handler.testMode
@@ -123,7 +125,7 @@ func (s *selectableTable) Render(ctx context.Context, handler *InputHandler, tit
 
 	// record the table in the handler for testing.
 	handler.table = s
-	handler.tableMu.Unlock()
+	s.tableMu.Unlock()
 
 	handler.setActive(true)
 	defer func() {
@@ -351,6 +353,9 @@ func (s *selectableTable) updateTableRows() {
 }
 
 func (s *selectableTable) handleRemoveEvent(remove RemoveMsg) (tea.Model, tea.Cmd) {
+	s.tableMu.Lock()
+	defer s.tableMu.Unlock()
+
 	newRows := make([][]string, 0, len(s.rawRows)-1)
 	for i, row := range s.rawRows {
 		if i == int(remove) {
@@ -372,6 +377,9 @@ func (s *selectableTable) handleRemoveEvent(remove RemoveMsg) (tea.Model, tea.Cm
 }
 
 func (s *selectableTable) handleInsertEvent(insert InsertMsg) (tea.Model, tea.Cmd) {
+	s.tableMu.Lock()
+	defer s.tableMu.Unlock()
+
 	s.rawRows = append(s.rawRows, insert)
 	s.filterRows(false)
 
@@ -379,6 +387,9 @@ func (s *selectableTable) handleInsertEvent(insert InsertMsg) (tea.Model, tea.Cm
 }
 
 func (s *selectableTable) handleDisableEvent(disable DisableMsg) (tea.Model, tea.Cmd) {
+	s.tableMu.Lock()
+	defer s.tableMu.Unlock()
+
 	// indexes to disable should always be absolute indexes and do not need to go through s.filterMap.
 	delete(s.activeRows, int(disable))
 	s.disabledRows[int(disable)] = true
@@ -387,6 +398,9 @@ func (s *selectableTable) handleDisableEvent(disable DisableMsg) (tea.Model, tea
 }
 
 func (s *selectableTable) handleEnableEvent(enable EnableMsg) (tea.Model, tea.Cmd) {
+	s.tableMu.Lock()
+	defer s.tableMu.Unlock()
+
 	// indexes to enable should always be absolute indexes and do not need to go through s.filterMap.
 	delete(s.disabledRows, int(enable))
 
@@ -394,6 +408,9 @@ func (s *selectableTable) handleEnableEvent(enable EnableMsg) (tea.Model, tea.Cm
 }
 
 func (s *selectableTable) handleKeyEvent(key tea.KeyMsg) (tea.Model, tea.Cmd) {
+	s.tableMu.Lock()
+	defer s.tableMu.Unlock()
+
 	switch key.Type {
 	case tea.KeyEsc:
 		fallthrough
@@ -494,4 +511,11 @@ func (s *selectableTable) rowStyle(row int) {
 
 		s.formatRows[row][col] = textStyle.String()
 	}
+}
+
+func (s *selectableTable) countRawRows() int {
+	s.tableMu.Lock()
+	defer s.tableMu.Unlock()
+
+	return len(s.rawRows)
 }
