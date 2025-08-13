7739ec6d3ef2a17f85eb38118a3f3166a1e9a099
Kadin Sayani <kadin.sayani@canonical.com>
2025-08-13 15:47:16 -0600

    service: Add `PassphraseWordCount` constant
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- service/test_wordlist.go
+++ service/test_wordlist.go
@@ -4,3 +4,6 @@ package service
 
 // Testing wordlist that will always print `a a a a`.
 var Wordlist = []string{"a", "a", "a", "a"}
+
+// PassphraseWordCount is the number of words in a passphrase.
+const PassphraseWordCount uint8 = 4
