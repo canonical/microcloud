79b4a9102f8643a7c0ee6abcd4b911dac311b76d
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-10-13 10:27:28 +0200

    database: Switch to Microcluster v3
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- database/cluster_manager.mapper.go
+++ database/cluster_manager.mapper.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/canonical/lxd/lxd/db/query"
 	"github.com/canonical/lxd/shared/api"
-	"github.com/canonical/microcluster/v2/cluster"
+	"github.com/canonical/microcluster/v3/cluster"
 )
 
 var _ = api.ServerEnvironment{}
