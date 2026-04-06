00c1c0270e803e19ddd37f258ecad5c3be281755
Minae Lee <minae.lee@canonical.com>
2026-04-06 07:58:44 -0700

    doc: add version number to sidebar and HTML title
    Signed-off-by: Minae Lee <minae.lee@canonical.com>
    (cherry picked from commit b3a848d5a0b5b90f5256feb001bfff6d81810255)

--- doc/custom_conf.py
+++ doc/custom_conf.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import re
 import yaml
 from redirects import redirects
 
@@ -25,10 +26,13 @@ from redirects import redirects
 project = 'MicroCloud'
 author = 'Canonical Group Ltd'
 
-# The title you want to display for the documentation in the sidebar.
-# You might want to include a version number here.
-# To not display any title, set this option to an empty string.
-html_title = ''
+with open('../version/version.go') as f:
+    match = re.search(r'RawVersion = "([^"]+)"', f.read())
+    version = match.group(1) if match else ''
+
+# Sidebar documentation title; best kept reasonably short
+# To disable the title, set to an empty string.
+html_title = project + ' documentation ' + version
 
 # The default value uses the current year as the copyright year.
 #
